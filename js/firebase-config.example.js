2d6e27fde2466d6bd291abe2d38576834c6e9b74
cooperativagloria4 <angeloscaqp16@gmail.com>
2026-03-08 10:43:26 -0500

    Add files via upload

---
+++ js/firebase-config.example.js
@@ -0,0 +1,22 @@
+// EJEMPLO DE CONFIGURACIÓN DE FIREBASE
+// Copia este archivo como 'js/firebase-config.js' y pon tus credenciales reales.
+
+export const firebaseConfigPrincipal = {
+    apiKey: "TU_API_KEY",
+    authDomain: "TU_PROYECTO.firebaseapp.com",
+    projectId: "TU_PROYECTO",
+    storageBucket: "TU_PROYECTO.firebasestorage.app",
+    messagingSenderId: "TU_SENDER_ID",
+    appId: "TU_APP_ID",
+    databaseURL: "https://TU_PROYECTO-default-rtdb.firebaseio.com" 
+};
+
+export const firebaseConfigCaja = {
+    apiKey: "TU_API_KEY_CAJA",
+    authDomain: "TU_PROYECTO_CAJA.firebaseapp.com",
+    projectId: "TU_PROYECTO_CAJA",
+    storageBucket: "TU_PROYECTO_CAJA.firebasestorage.app",
+    messagingSenderId: "TU_SENDER_ID_CAJA",
+    appId: "TU_APP_ID_CAJA",
+    databaseURL: "https://TU_PROYECTO_CAJA-default-rtdb.firebaseio.com" 
+};
