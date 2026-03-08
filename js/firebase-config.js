75642c50d700e384be19aa50523f069b51961c77
cooperativagloria4 <angeloscaqp16@gmail.com>
2026-03-08 00:47:21 -0500

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,20 @@
+// CONFIGURACIÓN DE FIREBASE (No subir este archivo a GitHub Público)
+export const firebaseConfigPrincipal = {
+    apiKey: "AIzaSyD9HtmP6EgriteFXerhnuOVC8DbtfGpSvY",
+    authDomain: "urbgloriaweb-1af85.firebaseapp.com",
+    projectId: "urbgloriaweb-1af85",
+    storageBucket: "urbgloriaweb-1af85.firebasestorage.app",
+    messagingSenderId: "965163716312",
+    appId: "1:965163716312:web:59182bbf9aaf2bc3f051ad",
+    databaseURL: "https://urbgloriaweb-1af85-default-rtdb.firebaseio.com" 
+};
+
+export const firebaseConfigCaja = {
+    apiKey: "AIzaSyD7vyFwUTUsb01rHCJ5PvepT70WFiVCqJ4",
+    authDomain: "urbgloriacaja.firebaseapp.com",
+    projectId: "urbgloriacaja",
+    storageBucket: "urbgloriacaja.firebasestorage.app",
+    messagingSenderId: "141426832510",
+    appId: "1:141426832510:web:61c7fcd68395f90075142f",
+    databaseURL: "https://urbgloriacaja-default-rtdb.firebaseio.com" 
+};
